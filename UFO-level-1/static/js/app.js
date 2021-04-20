7a1753ab7c88049f076b02ae859d5a1540345e43
Emm9-Hmm <72900380+Emm9-Hmm@users.noreply.github.com>
2021-04-19 22:12:19 -0500

    table added

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
 // YOUR CODE HERE!
+let tbody = d3.select("tbody");
+
+data.forEach((report) => {
+    let row = tbody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
